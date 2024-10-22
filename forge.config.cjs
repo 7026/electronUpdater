a3ef2bb15ed1e27ea1b5982be01703488e714e9b
MrYu <702600014@qq.com>
2024-10-22 14:52:04 +0800

    v1.0.10

---
+++ forge.config.cjs
@@ -0,0 +1,47 @@
+/*
+ * @Author       : MrYu
+ * @Date         : 2024-10-22 09:59:03
+ * @LastEditors  : MrYu
+ * @LastEditTime : 2024-10-22 14:51:12
+ * @FilePath     : /updater/forge.config.cjs
+ */
+module.exports = {
+  // publishers: [
+  //   {
+  //     name: '@electron-forge/publisher-github',
+  //     config: {
+  //       repository: {
+  //         owner: 'MrYu',
+  //         name: 'electronUpdater'
+  //       },
+  //       prerelease: false,
+  //       draft: true
+  //     }
+  //   }
+  // ],
+  // packagerConfig: {
+  //   ignore: [
+  //     /^\/src/,
+  //     /(.eslintrc.json)|(.gitignore)|(electron.vite.config.ts)|(forge.config.cjs)|(tsconfig.*)/
+  //   ]
+  // },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin']
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {}
+    }
+  ]
+}
